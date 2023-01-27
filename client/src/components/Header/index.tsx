3bed0106f662c5df070d7a7c2e4471e6600c9145
jv-vogler <jvsvogler@gmail.com>
2023-01-27 11:40:50 -0300

    Add styling to JobCard and Header components

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,6 +1,68 @@
+import { FaSearch } from 'react-icons/fa';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-around;
+  place-items: center;
+  height: 100%;
+  padding: 2rem 0;
+  position: sticky;
+  top: 0;
+  z-index: 100;
+  color: #f5f5f5;
+  background-color: #333;
+
+  @media screen and (max-width: 768px){
+    flex-direction: column;
+    gap: 20px;
+  }
+`;
+
+const Input = styled.input`
+  height: 44px;
+  padding: 6px 10px;
+  font-size: 1rem;
+  border: none;
+  outline: none;
+  border-top-left-radius: 3px;
+  border-bottom-left-radius: 3px;
+`;
+
+const SearchBtn = styled.button`
+  height: 44px;
+  padding: 0px 15px;
+  border: none;
+  outline: none;
+  border-top-right-radius: 3px;
+  border-bottom-right-radius: 3px;
+  color: #fff;
+  background-color: #ff6000;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #e23c00;
+  }
+`
+
+const FilterBtn = styled.button`
+  padding: 0.5rem 1rem;
+  border: 1px solid #fff;
+  color: #fff;
+  background-color: #333;
+  cursor: pointer;
+`
+
 const Header: React.FC = () => {
   return (
-    <header>Header</header>
-  )
-}
-export default Header
+    <StyledHeader>
+      <h1>Zippia Test</h1>
+      <div>
+        <Input type="text" placeholder="Company name" />
+        <SearchBtn><FaSearch /></SearchBtn>
+      </div>
+      <FilterBtn>Past 7 days</FilterBtn>
+    </StyledHeader>
+  );
+};
+export default Header;
--- client/src/components/JobCard/index.tsx
+++ client/src/components/JobCard/index.tsx
@@ -1,3 +1,34 @@
+/**
+ * Since this is a very simple project I'm using the Styled components within the same file.
+ * 
+ * Also added the expected Props type toe the JobCard component to get autocompletion when creating
+ * them.
+ */
+
+import styled from 'styled-components';
+
+const Li = styled.li`
+  display: flex;
+  flex-direction: column;
+  padding: 2rem 3rem;
+`
+
+const H1 = styled.h1`
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
+
+const H2 = styled.h2`
+  font-size: 1rem;
+  font-weight: bold;
+`;
+
+const P = styled.p`
+  padding-top: 1rem;
+  color: #777;
+  font-weight: 400;
+`;
+
 type Props = {
   jobTitle: string;
   jobCompany: string;
@@ -6,11 +37,11 @@ type Props = {
 
 const JobCard: React.FC<Props> = ({ jobTitle, jobCompany, jobDescription }) => {
   return (
-    <li>
-      <h1>{jobTitle}</h1>
-      <h2>{jobCompany}</h2>
-      <p>{jobDescription}</p>
-    </li>
+    <Li>
+      <H1>{jobTitle}</H1>
+      <H2>{jobCompany}</H2>
+      <P>{jobDescription}</P>
+    </Li>
   );
 };
 export default JobCard;
