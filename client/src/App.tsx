f1e1c523193c45446eadc8205230856a36b0479f
jv-vogler <jvsvogler@gmail.com>
2023-01-27 10:49:28 -0300

    Add JobsList component and handle the data fetching

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,28 @@
+/**
+ * I decided to use react-query for the data fetching and management so I don't have to worry about
+ * caching or using other hooks like useEffect and useState.
+ * 
+ * For styling I chose styled-components as it is the tool I'm more comfortable with and I set the
+ * GlobalStyle for css resets such as margin, padding and box-sizing, and also setting fonts and such.
+ * 
+ * I opted for a very simple component structure to begin with since I'm worried about time.
+ */
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+import GlobalStyle from './styles/globals';
 import Header from './components/Header';
 import JobsList from './components/JobsList';
-import GlobalStyle from './styles/globals';
+
+const queryClient = new QueryClient();
 
 const App: React.FC = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyle />
       <Header />
       <JobsList />
-    </>
+    </QueryClientProvider>
   );
 };
 export default App;
--- client/src/components/JobsList/index.tsx
+++ client/src/components/JobsList/index.tsx
@@ -1,9 +1,52 @@
+/**
+ * I chose a 'spinner' library to give a visual feedback to the user while data is being fetched.
+ * Also displaying an error message in case the fetching fails.
+ *
+ * It would have been great if I used libraries like jest and react-testing-library to test this
+ * component instead of making the API calls directly, but I still need more experience with that.
+ *
+ * At least the problems of this is minimized by react-query's caching and vite's hot reloading.
+ */
+
+import { useQuery } from 'react-query';
+import { MagnifyingGlass } from 'react-loader-spinner';
 import JobCard from '../JobCard';
 
 const JobsList: React.FC = () => {
+  const { data, error, isLoading } = useQuery('jobsData', async () => {
+    const res = await fetch('/test/jobs');
+    return res.json();
+  });
+
+  if (isLoading) {
+    return (
+      <MagnifyingGlass
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="MagnifyingGlass-loading"
+        wrapperStyle={{}}
+        wrapperClass="MagnifyingGlass-wrapper"
+        glassColor="#c0efff"
+        color="#e15b64"
+      />
+    );
+  }
+
+  if (error) return <p>Something went wrong...</p>;
+
   return (
     <ul>
-      <JobCard jobTitle="title" jobCompany="company" jobDescription="desc" />
+      {data.jobs.map(
+        (job: { jobId: string; jobTitle: string; companyName: string; jobDescription: string }) => (
+          <JobCard
+            key={job.jobId}
+            jobTitle={job.jobTitle}
+            jobCompany={job.companyName}
+            jobDescription={job.jobDescription}
+          />
+        )
+      )}
     </ul>
   );
 };
